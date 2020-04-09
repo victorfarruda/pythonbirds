4fae01ddca86221117dbf3f326ec82302b31b2b4
Victor Arruda <victorf.arruda@outlook.com>
2020-04-09 13:47:56 -0300

    Criando métodos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 
 if __name__ == '__main__':
     victor = Pessoa(nome='Victor')
@@ -31,3 +39,5 @@ if __name__ == '__main__':
     print(luciano.olhos)
     print(victor.olhos)
     print(id(Pessoa.olhos), id(victor.olhos), id(luciano.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
